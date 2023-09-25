52fce566e1705cc8ffb89f27dcc9c977b0010b1f
Hseng Hurk <bin4ryhunt3r@gmail.com>
2023-09-26 00:31:53 +0700

    updated commit

--- src/chapter_2/Literal.java
+++ src/chapter_2/Literal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chapter2;
+package chapter_2;
 
 //This is just comment
 //This is another line
