12f1fc0b206bbb2e5f40f357efe7a94dcbc6a00c
Hseng Hurk <bin4ryhunt3r@gmail.com>
2023-09-29 21:00:55 +0700

    updated commit

--- src/chapter_27/ImageDemo.java
+++ src/chapter_27/ImageDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chapter27;
+package chapter_27;
 
 import java.awt.Graphics;
 import java.awt.Image;
